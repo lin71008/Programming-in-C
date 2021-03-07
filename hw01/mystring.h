59fadf7ee7d989c5cf019c972372d46f89256ad5
lin71008 <lin71008@gmail.com>
2021-03-08 00:34:23 +0800

    HW1: Problem 5

--- hw01/mystring.h
+++ hw01/mystring.h
@@ -1,22 +1,136 @@
 #ifndef __MYSTRING_H__
 #define __MYSTRING_H__
 
+#include <stdio.h>
 #include <string.h>
 
-// char* mystrchr(const char *s, int c);
-// char* mystrrchr(const char *s, int c);
-// size_t mystrspn(const char *s, const char *accept);
-// size_t mystrcspn(const char *s, const char *reject);
-// char* mystrpbrk(const char *s, const char *accept);
-// char* mystrstr(const char *haystack , const char *needle);
-// char* mystrtok(char *str, const char *delim);
+char* mystrchr(const char *s, int c)
+{
+    if (s == NULL) return NULL;
+    while (*s != '\0')
+    {
+        if (*s == c) return (char*) s;
+        s++;
+    }
+    return NULL;
+}
 
-#define mystrchr strchr
-#define mystrrchr strrchr
-#define mystrspn strspn
-#define mystrcspn strcspn
-#define mystrpbrk strpbrk
-#define mystrstr strstr
-#define mystrtok strtok
+char* mystrrchr(const char *s, int c)
+{
+    if (s == NULL) return NULL;
+    const char *p = NULL;
+    while (*s != '\0')
+    {
+        if (*s == c) p = s;
+        s++;
+    }
+    return (char*) p;
+}
+
+size_t mystrspn(const char *s, const char *accept)
+{
+    if (s == NULL || accept == NULL) return 0;
+    size_t n = 0;
+    const char *p;
+    while (*s != '\0')
+    {
+        p = accept;
+        while (*p != '\0')
+        {
+            if (*s == *p) break;
+            p++;
+        }
+        if (*s != *p) break;
+        n++; s++;
+    }
+    return n;
+}
+
+size_t mystrcspn(const char *s, const char *reject)
+{
+    if (s == NULL || reject == NULL) return 0;
+    size_t n = 0;
+    const char *p;
+    while (*s != '\0')
+    {
+        p = reject;
+        while (*p != '\0')
+        {
+            if (*s == *p) break;
+            p++;
+        }
+        if (p != NULL && *p != '\0') break;
+        n++; s++;
+    }
+    return n;
+}
+
+char* mystrpbrk(const char *s, const char *accept)
+{
+    if (s == NULL || accept == NULL) return NULL;
+    const char *p;
+    while (*s != '\0')
+    {
+        p = accept;
+        while (*p != '\0')
+        {
+            if (*s == *p) return (char*) s;
+            p++;
+        }
+        s++;
+    }
+    return NULL;
+}
+
+char* mystrstr(const char *haystack , const char *needle)
+{
+    if (haystack == NULL || needle == NULL) return NULL;
+    const char *ph, *pn;
+    while (*haystack != '\0')
+    {
+        ph = haystack; pn = needle;
+        while (*pn != '\0')
+        {
+            if (*ph == '\0' || *ph != *pn) break;
+            ph++; pn++;
+        }
+        if (*pn == '\0') return (char*) haystack;
+        haystack++;
+    }
+    return NULL;
+}
+
+static char *last = NULL;
+char* mystrtok(char *str, const char *delim)
+{
+    if ((str == NULL && last == NULL) || delim == NULL) return NULL;
+    if (str == NULL) str = last;
+    char *s = str; const char *p;
+    while (*s != '\0')
+    {
+        p = delim;
+        while (*p != '\0')
+        {
+            if (*s == *p)
+            {
+                if (s == str)
+                {
+                    str++;
+                    break;
+                }
+                else
+                {
+                    *s = '\0';
+                    last = s+1;
+                    return str;
+                }
+            }
+            p++;
+        }
+        s++;
+    }
+    if (*s == '\0') last = NULL;
+    return str;
+}
 
 #endif
