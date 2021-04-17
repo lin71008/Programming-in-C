c8662cd6acdc4c3ceb4fc8f64ed73451f03b68e8
lin71008 <lin71008@gmail.com>
2021-04-17 14:22:02 +0800

    HW3: Problem 1
    Well... sleep always have delay...

--- hw03/hw0301.c
+++ hw03/hw0301.c
@@ -2,22 +2,67 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <unistd.h>
 #include <errno.h>
 #include <time.h>
 
+#include "share.h"
+
 #ifdef _WIN32
-    // sleep
-    #include <unistd.h>
-    // color
-    #include <windows.h>
+// color
+#include <windows.h>
 #else
-    // sleep
-    #include <sys/time.h>
-    // color using ANSI
+// nanosleep, gettimeofday
+#include <sys/time.h>
+#include "color.h"
 #endif
 
-#include "share.h"
-#include "color.h"
+#ifdef _WIN32
+
+// lyrics color setting
+const int32_t DefaultColor = 15;
+const int32_t LyricsColor[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+const int32_t LyricsColor_MAX = sizeof(LyricsColor) / sizeof(int32_t);
+
+void set_color(int32_t n)
+{
+    HANDLE  hConsole;
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (n == -1)
+    {
+        SetConsoleTextAttribute(hConsole, DefaultColor);
+    }
+    else if (n < LyricsColor_MAX)
+    {
+        SetConsoleTextAttribute(hConsole, LyricsColor[n]);
+    }
+}
+
+void sleep_ms(int32_t delay)
+{
+    sleep(delay / 1000);
+}
+
+int gettimeofday(struct timeval *tp, void *tzp)
+{
+  time_t clock;
+  struct tm tm;
+  SYSTEMTIME wtm;
+  GetLocalTime(&wtm);
+  tm.tm_year   = wtm.wYear - 1900;
+  tm.tm_mon   = wtm.wMonth - 1;
+  tm.tm_mday   = wtm.wDay;
+  tm.tm_hour   = wtm.wHour;
+  tm.tm_min   = wtm.wMinute;
+  tm.tm_sec   = wtm.wSecond;
+  tm. tm_isdst  = -1;
+  clock = mktime(&tm);
+  tp->tv_sec = clock;
+  tp->tv_usec = wtm.wMilliseconds * 1000;
+  return 0;
+}
+
+#else
 
 // lyrics color setting
 const char *DefaultColor = CLR_LWHI;
@@ -34,37 +79,23 @@ const int32_t LyricsColor_MAX = sizeof(LyricsColor) / sizeof(char*);
 
 void set_color(int32_t n)
 {
-    #ifdef _WIN32
-        HANDLE  hConsole;
-        hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-        if (n == -1)
-        {
-            SetConsoleTextAttribute(hConsole, 15);
-        }
-        else if (n < LyricsColor_MAX)
-        {
-            SetConsoleTextAttribute(hConsole, n+1);
-        }
-    #else
-        if (n == -1)
-        {
-            printf("%s", DefaultColor);
-        }
-        else if (n < LyricsColor_MAX)
-        {
-            printf("%s", LyricsColor[n]);
-        }
-    #endif
+    if (n == -1)
+    {
+        printf("%s", DefaultColor);
+    }
+    else if (n < LyricsColor_MAX)
+    {
+        printf("%s", LyricsColor[n]);
+    }
 }
+
 void sleep_ms(int32_t delay)
 {
-    #ifdef _WIN32
-        sleep(delay / 1000);
-    #else
-        struct timespec sDelay = {.tv_sec = (delay / 100), .tv_nsec = (delay % 100) * 1000};
-        while (nanosleep(&sDelay, &sDelay) == -1 && errno != EINTR) { /* waiting */ }
-    #endif
+    struct timespec sDelay = {.tv_sec = (delay / 1000), .tv_nsec = (delay % 1000) * 1000};
+    while (nanosleep(&sDelay, &sDelay) == -1 && errno != EINTR) { /* waiting */ }
 }
+
+#endif
 
 int main()
 {
@@ -92,9 +123,11 @@ int main()
     size_t len = 0;  // buffer size
     ssize_t read;  // line len
 
-    time_t cur = clock();
     uint32_t m = 0, s = 0, x = 0;  // current lyrics time
     uint32_t lm = 0, ls = 0, lx = 0;  // last lyrics time
+
+    struct  timeval origin, current;  // debug only...
+    gettimeofday(&origin, NULL);
 
     char *name_buffer[LyricsColor_MAX];
     int32_t top = 0;
@@ -108,7 +141,12 @@ int main()
             int32_t delay = (60000*m+1000*s+x)-(60000*lm+1000*ls+lx);  // [ms]
             sleep_ms(delay);
             remove_newline(lyrics);
-            printf("[%02ld:%02ld.%02ld]: %s\n", (clock()-cur)/CLOCKS_PER_SEC/60, ((clock()-cur)/CLOCKS_PER_SEC)%60,((clock()-cur)/CLOCKS_PER_SEC*100)%100, lyrics);
+
+            // gettimeofday(&current, NULL);
+            // uint64_t diff = (1000000*current.tv_sec+current.tv_usec)-(1000000*origin.tv_sec+origin.tv_usec);
+            // printf("[%02d:%02d.%02d]: ", (uint32_t)(diff/60000000), (uint32_t)((diff/1000000)%60), (uint32_t)((diff/1000)%100));
+
+            printf("%s\n", lyrics);
             lm = m, ls = s, lx = x;
         }
         else if (line[read-2] == ':')
