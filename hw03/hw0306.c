85fcfa21fc7f447c7279fa331e35aca52583b3e0
lin71008 <lin71008@gmail.com>
2021-04-29 12:46:16 +0800

    HW3: Problem 6

--- hw03/hw0306.c
+++ hw03/hw0306.c
@@ -3,8 +3,7 @@
 
 void max(int32_t a, int32_t b)
 {
-    if (a > b) printf("%d\n", a);
-    else printf("%d\n", b);
+    printf("%d\n", a ^ ((a ^ b) & -(a < b)));
 }
 
 int main()
@@ -12,23 +11,23 @@ int main()
     max(100, 50);
     max(50, 100);
 
-    max(-100, -50);
     max(-50, -100);
+    max(-100, -50);
 
     max(100, -50);
     max(-50, 100);
 
-    max(-100, 50);
     max(50, -100);
+    max(-100, 50);
 
     max(INT32_MAX, 0);
     max(0, INT32_MAX);
 
-    max(INT32_MIN, 0);
     max(0, INT32_MIN);
+    max(INT32_MIN, 0);
 
-    max(INT32_MIN, INT32_MAX);
     max(INT32_MAX, INT32_MIN);
+    max(INT32_MIN, INT32_MAX);
 
     return 0;
 }
